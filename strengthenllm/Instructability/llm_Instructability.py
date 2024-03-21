57142e3e3a0843d14258bafa4fbfb425974d4728
han-shanshan <sshan0731@hotmail.com>
2024-03-20 18:42:18 -0700

    multi task

--- strengthenllm/Instructability/llm_Instructability.py
+++ strengthenllm/Instructability/llm_Instructability.py
@@ -1,6 +1,6 @@
 # https://www.nsf.gov/pubs/2023/nsf23610/nsf23610.htm
 
-# 2) Instructibility. Taking advantage of this firmer understanding, strengthened AI must be “instructible”.
+# 2) Instructability. Taking advantage of this firmer understanding, strengthened AI must be “instructible”.
 # This means that systems can be proven experimentally to change their behavior appropriately in response
 # to explicit feedback provided by even non-expert users. Related is how such instructible systems might invert
 # the mechanisms behind this principle to implement more effective and trustworthy assistance to humans
