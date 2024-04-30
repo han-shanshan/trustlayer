004b5aacf26a922dd5abdcb37f21ad40bd62e579
han-shanshan <sshan0731@hotmail.com>
2024-04-30 16:47:13 -0700

    safety detection tasks + fox

---
+++ multitask_lora/fox/topic_fox.py
@@ -0,0 +1,25 @@
+from multitask_lora.constants import TOPIC_TASK_NAME, FOX_BASE_GPU
+from multitask_lora.trust_inference_engine import TrustInferenceEngine
+from multitask_lora.training_engine import TrainingEngine
+import os
+import torch
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+
+TASK_NAME = TOPIC_TASK_NAME
+MODEL_NAME = FOX_BASE_GPU  # "google-bert/bert-base-uncased"
+lora_storage_path = MODEL_NAME.split("/")[1]
+OUTPUT_DIR = lora_storage_path + "-" + TASK_NAME
+
+"""
+training data: https://huggingface.co/datasets/deepset/prompt-injections
+"""
+
+if __name__ == '__main__':
+    # https://huggingface.co/docs/transformers/main/en/peft
+    trainer = TrainingEngine(base_model_name=MODEL_NAME, task_name=TASK_NAME)
+    trainer.train()
+    # text = "i'm happy hahaha"
+    #
+    # inference_engine = InferenceEngine(default_task=TASK_NAME)
+    # print(inference_engine.inference(text))
