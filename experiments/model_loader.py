93d0644f1cc23aad200cfe60ca9a5ad925b07908
han-shanshan <sshan0731@hotmail.com>
2024-05-11 21:10:14 -0700

    model loader adds fox and llama3

--- experiments/model_loader.py
+++ experiments/model_loader.py
@@ -1,10 +1,13 @@
 from transformers import pipeline
 from data_operation.data_reader import DataReader
 import torch
+from multitask_lora.constants import FOX_BASE_GPU
 
 TINYLLAMA_MODEL = "tinyllama"
 MISTRAL7B_MODEL = "mistral-7b"
 FALCON40B_MODEL = "falcon-40"
+LLAMA3_8B_MODEL = "llama3-8b"
+FOX_MODEL = "fox"
 
 
 class ModelLoader:
@@ -21,6 +24,15 @@ class ModelLoader:
         if model_name == TINYLLAMA_MODEL:
             return pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", device=5)
         if model_name == FALCON40B_MODEL:
-            return pipeline("text-generation", model="tiiuae/falcon-40b-instruct", torch_dtype=torch.bfloat16,
-                            max_new_tokens=1024, device_map="auto")
+            pipeline("text-generation", model="tiiuae/falcon-40b", torch_dtype=torch.bfloat16,
+                     max_new_tokens=1024, device=6)
 
+        if model_name == FOX_MODEL:
+            return pipeline("text-generation", model=FOX_BASE_GPU)
+        if model_name == LLAMA3_8B_MODEL:
+            hf_key = DataReader().read_hf_apikey()
+            return pipeline("text-generation", model="meta-llama/Meta-Llama-3-8B", token=hf_key)
+
+
+if __name__ == '__main__':
+    ModelLoader.load_model(FALCON40B_MODEL)
